b626d637d8f3470afc65dad7504e578d29feac77
Brandon <ratchetnclank@gmail.com>
2024-07-03 18:03:15 -0700

    Continued

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 bool validateTitle (int defaults[], int checker[]);
-u8 get_cart_type(ifstream &romRead);
+string get_cart_type(ifstream &romRead);
 
 static int bitmap[48] = {
     0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
@@ -16,11 +16,13 @@ static int bitmap[48] = {
     0xBB, 0xBB, 0x67, 0x63, 0x6E, 0x0E, 0xEC, 0xCC, 0xDD, 0xDC, 0x99, 0x9F, 0xBB, 0xB9, 0x33, 0x3E,
 };
 
+string romType;
+
 int main() {
     char title[15];
     char romsets[48];
     int convRomset[48];
-    int cartType;
+    string cartType;
 
     ifstream romRead;
     string filename;
@@ -76,7 +78,7 @@ bool validateTitle (int defaults[], int checker[]) {
     return true;
 }
 
-u8 get_cart_type(ifstream &romRead) {
+string get_cart_type(ifstream &romRead) {
     char temp[2];
     int type;
     
@@ -85,9 +87,28 @@ u8 get_cart_type(ifstream &romRead) {
 
     type = u8(temp[0]);
     cout << hex << "Carttype value is: 0x" << type;
-    /*switch (temp) {
-        case 0x00 : 
-    } */
 
-    return temp[0];
+    switch (type) {
+        case 0x00 : 
+            return "ROM ONLY";
+        case 0x01 :
+            return "MBC1";
+        case 0x02 :
+            return "MBC1+RAM";
+        case 0x03 :
+            return "MBC1+RAM+BATTERY";
+        case 0x05 :
+            return "MBC2";
+        case 0x06 :
+            return "MBC2+BATTERY";
+        case 0x08 :
+            return "ROM+RAM ";
+        case 0x09 :
+            return "ROM+RAM+BATTERY";
+        case 0x0B :
+            return "MMM01";
+            
+    } 
+
+    //return temp[0];
 }
