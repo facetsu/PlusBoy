958e660cba6b303b595e63dffa311a07a52ad31c
Brandon <ratchetnclank@gmail.com>
2024-07-04 02:14:31 -0700

    ROM size implementation

--- test.cpp
+++ test.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 bool validateTitle (int defaults[], int checker[]);
 string get_cart_type(ifstream &romRead);
+string get_cart_size(ifstream &romRead);
 
+/* This is default Gameboy title which all cartridges must
+    pass a check agaisnt for the bootloader to continue.
+    If the cartridhe does not match this hexdump at the specified
+    address, the bootrom locks up and the Gameboy must be reset*/
 static int bitmap[48] = {
     0xCE, 0xED, 0x66, 0x66, 0xCC, 0x0D, 0x00, 0x0B, 0x03, 0x73, 0x00, 0x83, 0x00, 0x0C, 0x00, 0x0D,
     0x00, 0x08, 0x11, 0x1F, 0x88, 0x89, 0x00, 0x0E, 0xDC, 0xCC, 0x6E, 0xE6, 0xDD, 0xDD, 0xD9, 0x99,
@@ -20,10 +25,11 @@ static int bitmap[48] = {
 string romType;
 
 int main() {
-    char title[15];
+    char title[16];
     char romsets[48];
     int convRomset[48];
     string cartType;
+    string cartSize;
     ifstream romRead;
     string filename;
     int temp1;
@@ -39,7 +45,7 @@ int main() {
 
     romRead.seekg(0x0134, ios::beg);         //places our read pointer at the starting position for cart titles (0x0134)
     romRead.read(title, 0x10);               // reads the cart title. they must always fit into 0xF long bytes (16 bytes).
-    cout << "The internal cartridge title is: " << title << endl;
+    cout << "The internal cartridge title is: " << title << endl << endl;
 
     romRead.seekg(0x0104, ios::beg);
     romRead.read(romsets, 0x30);
@@ -54,15 +60,20 @@ int main() {
     }
 
     cout << endl;
-    cout << "The bootcheck sum to be checked against is: ";
+    cout << "The required bootsum is:                    ";
     for (int i = 0; i < 48; i++) {
         cout << int(bitmap[i]) << " ";
     }
     cout << endl;
 
     validateTitle(bitmap, convRomset);
+
     cartType = get_cart_type(romRead);
-    cout << "Our cart's type is " << cartType << endl;
+    cout << "Our cart's type is: " << cartType << endl;
+
+    cartSize = get_cart_size(romRead);
+    cout << "Our cart's size is " << cartSize << endl;
+
     
     return 0;
 }
@@ -70,11 +81,11 @@ int main() {
 bool validateTitle (int defaults[], int checker[]) {
     for(int i = 0; i < 48; i++) {
         if (checker[i] != defaults[i]) {
-            cout << "The title hexdump does not match! Not a genuine cartridge." << endl;
+            cout << "The title hexdump does not match! Not a genuine cartridge." << endl << endl;
             return false;
         }
     }
-    cout << "The cartridge title hexdump is validated. Continuing." << endl;
+    cout << endl << "The cartridge title hexdump is validated. Continuing." << endl;
     return true;
 }
 
@@ -86,7 +97,7 @@ string get_cart_type(ifstream &romRead) {
     romRead.read(temp, 1);
 
     type = u8(temp[0]);
-    cout << hex << "Cart-type hex is: 0x" << type << endl;
+    cout << hex << "Cart-type hex is: 0x" << type << endl << endl;
 
     switch (type) {
         case 0x00 : 
@@ -149,4 +160,45 @@ string get_cart_type(ifstream &romRead) {
             return "ERROR";
     } 
     return "FAILURE";
+}
+
+string get_cart_size(ifstream &romRead) {
+    char temp[2];
+    int size;
+
+    romRead.seekg(0x0148, ios::beg);   
+    romRead.read(temp, 1);
+
+    size = u8(temp[0]);
+    cout << hex << "Cart-size hex is: 0x" << size << endl << endl;
+    
+    switch (size) {
+        case 0x00 :
+            return "32 KiB | 2 ROM BANKS";
+        case 0x01 :
+            return "64 KiB | 4 ROM BANKS";
+        case 0x02 :
+            return "128 KiB | 8 ROM BANKS";
+        case 0x03 :
+            return "256 KiB | 16 ROM BANKS";
+        case 0x04 :
+            return "512 KiB | 32 ROM BANKS";
+        case 0x05 :
+            return "1 MiB | 64 ROM BANKS";
+        case 0x06 :
+            return "2 MiB | 128 ROM BANKS";
+        case 0x07 :
+            return "4 MiB | 256 ROM BANKS";
+        case 0x08 :
+            return "8 MiB | 512 ROM BANKS";
+        case 0x52 :
+            return "1.1 MiB | 72 ROM BANKS";
+        case 0x53 :
+            return "1.2 MiB | 80 ROM BANKS";
+        case 0x54 :
+            return "1.5 MiB | 96 ROM BANKS";
+        default :
+            return "ERROR";
+    }
+    return "ERROR";
 }
